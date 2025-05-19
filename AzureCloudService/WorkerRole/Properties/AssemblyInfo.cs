240939e232f69ac807434dd18332ac199edddb38
piw@microsoft.com <piw@microsoft.com>
2025-05-19 17:09:42 +0800

    add worker role

--- AzureCloudService/WorkerRole/Properties/AssemblyInfo.cs
+++ AzureCloudService/WorkerRole/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WorkerRole1")]
+[assembly: AssemblyTitle("WorkerRole")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WorkerRole1")]
+[assembly: AssemblyProduct("WorkerRole")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6c73cefc-4f05-4cb5-ada2-deddc004f1a0")]
+[assembly: Guid("76b14977-0097-4542-8b35-18bc66fa356a")]
 
 // Version information for an assembly consists of the following four values:
 //
